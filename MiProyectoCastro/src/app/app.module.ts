98c17980b7a0b1fd1e4e0c5fb7e49500152a10f0
Alberto35018 <alberto.emmanuel.g35@gmail.com>
2022-07-14 06:45:18 -0300

    mi proyecto

--- MiProyectoCastro/src/app/app.module.ts
+++ MiProyectoCastro/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    AboutComponent
+
   ],
   imports: [
     BrowserModule,
