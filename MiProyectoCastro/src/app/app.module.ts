1225b35d163daa8f98fff170e5012ddb8f023367
Alberto35018 <alberto.emmanuel.g35@gmail.com>
2022-07-14 23:48:56 -0300

    seccion proyectos

--- MiProyectoCastro/src/app/app.module.ts
+++ MiProyectoCastro/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { AboutComponent } from './components/about/about.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProjectComponent } from './components/project/project.component';
 
 
 @NgModule({
@@ -15,12 +21,18 @@ import { AboutComponent } from './components/about/about.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    AboutComponent
+    AboutComponent,
+    BannerComponent,
+    ExperienceComponent,
+    EducationComponent,
+    HardAndSoftSkillsComponent,
+    ProjectComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
